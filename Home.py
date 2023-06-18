36cbd933b7163d48277c0aa16d12d013f936fdd1
Henrique Ferreira <msferreirahenrique@gmail.com>
2023-06-18 13:56:10 -0300

    Incluindo os arquivos iniciais para o desenvolvimento do site

---
+++ Home.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Henrique Ferreira", page_icon = "")
+
+st.markdown(
+    """
+
+    ## Apresentação Inicial
+
+    Me Chamo Henrique Ferreira, sou graduado em Ciências Econômicas pela Universidade Federal do Paraná e atualmente estou cursando uma pós graduação de Data Science e Big Data também na UFPR. 
+
+    Trabalho com análise de dados desde 2019, inicialmente em áreas voltadas à finanças, e agora atuo como Analytics Engineer, construindo e gerenciando a plataforma de dados da empresa para proporcionar um ambiente de trabalho mais eficiente para o time de analista de dados da minha área
+
+
+
+    """
+
+)
+
+st.markdown(
+    """
+
+    ## Links Importantes:
+
+    - [Linkedin](https://www.linkedin.com/in/henriquemsferreira/)
+
+    - [Github](https://github.com/henriquemsf)
+
+    """
+
+)
