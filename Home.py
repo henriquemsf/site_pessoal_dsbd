6c180f0b1b3fcf9b56ddde84cb78ad87bfe8f272
Henrique Ferreira <msferreirahenrique@gmail.com>
2023-06-18 14:14:57 -0300

    Criação e desenvolvimento da página de vida acadêmica

--- Home.py
+++ Home.py
@@ -9,9 +9,13 @@ st.markdown(
 
     Me Chamo Henrique Ferreira, sou graduado em Ciências Econômicas pela Universidade Federal do Paraná e atualmente estou cursando uma pós graduação de Data Science e Big Data também na UFPR. 
 
-    Trabalho com análise de dados desde 2019, inicialmente em áreas voltadas à finanças, e agora atuo como Analytics Engineer, construindo e gerenciando a plataforma de dados da empresa para proporcionar um ambiente de trabalho mais eficiente para o time de analista de dados da minha área
+    Trabalho com análise de dados desde 2019, inicialmente focado na área de finanças. Essa experiência me proporcionou um profundo entendimento dos desafios enfrentados pelas empresas nesse setor e como a análise de dados pode impulsionar a tomada de decisões estratégicas.
+    
+    Atualmente, atuo como Analytics Engineer, construindo e gerenciando a plataforma de dados da empresa em que trabalho. Meu objetivo é fornecer um ambiente de trabalho mais eficiente para o time de analistas de dados, capacitando-os a extrair insights valiosos e acionáveis a partir dos dados disponíveis. Trabalho em estreita colaboração com as equipes para entender suas necessidades e implementar soluções eficazes que melhorem a qualidade e a velocidade das análises.
 
+    Ao longo da minha jornada profissional, tive a oportunidade de trabalhar com diversas ferramentas e tecnologias de análise de dados, incluindo Python, R, SQL, Metabase e com o Google Cloud Platform (GCP). Essas habilidades técnicas combinadas com meu conhecimento em economia me permitem ter uma visão abrangente dos desafios de negócios e fornecer insights estratégicos que agregam valor real às organizações.
 
+    No menu lateral do meu site, você encontrará mais informações sobre minha trajetória, projetos anteriores e habilidades específicas. Estou sempre em busca de novos desafios e oportunidades para aplicar meu conhecimento em análise de dados de forma criativa e impactante.
 
     """
 
---
+++ pages/Acadêmico.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("Vida Acadêmica")
+
+st.markdown(
+            """
+
+            ### Pós em Data Science & Big Data (UFPR) 2023-2024
+            
+            - Por meio desse curso de especialização espero conseguir agregar ainda mais valor nas análises de dados que realizo no meu dia a dia profissional. 
+
+            - Além disso, acredito que ela aumentará minha bagagem acadêmica para buscar iniciar um curso de Mestrado após a sua conclusão.
+
+            - Mais informações sobre o curso podem ser encontradas nesse [link](http://dsbd.leg.ufpr.br/)
+            
+            """
+        )
+
+st.markdown(
+            """
+
+            ### Graduação em Ciências Econômicas (UFPR) 2017-2021
+
+
+            - Meu trabalho de conclusão de curso foi voltado às áreas de Economia Comportamental e Economia da Saúde, explorando os principais motivos que levam as pessoas a não se tornarem doadoras de órgãos e o impacto disso nas contas públicas devido à sobrecarga no SUS, sobretudo em casos de transplante renal. O trabalho pode ser acessado por meio desse [link](https://acervodigital.ufpr.br/handle/1884/76555)
+            - Durante o período da minha graduação, tive a oportunidade de trabalhar voluntariamente para a Empresa Júnior do meu setor (JR Consultoria), onde fiquei por dois anos realizando trabalhos de consultoria voltados à finanças, estratégia e marketing para empresas de pequeno e médio porte.
+            - Também fui responsável por toda a área financeira da empresa no meu segundo ano, gerenciando um faturamento anual de aproximadamente R$ 150.000,00
+            
+            """
+        )
